3610445a5b877d9e9459b60c9d2a1e0cc076217c
sohyeon <sohyun9743@naver.com>
2022-05-13 15:19:35 +0900

    update : error 해결

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+import {auth} from 'fbase';
+import Auth from 'routes/Auth';
 
-const App = () => {
+function App () {
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
   return (
-    <AppRouter />
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <Auth />
+      <footer>&copy; nwitter {new Date().getFullYear()} nwitter</footer>
+    </>
   );
 };
+
+//function App을 사용하고 그 안에 AppRouter를 사용하는 이유는?
+//footer를 사용하고 싶을 수 있기 때문.
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Auth = () => {
     return (
         <div>
-            
+            h2
+            df
+            <p>test</p>
+            <form>
+                <input type="text" />
+            </form>
         </div>
     );
 };
