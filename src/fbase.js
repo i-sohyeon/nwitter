e2a622fb9fac7f28ca588b72f4026493f588764e
sohyeon <sohyun9743@naver.com>
2022-06-15 23:47:18 +0900

    update : firebase import추가

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,5 +15,5 @@ const firebaseConfig = {
 export default firebase.initializeApp(firebaseConfig);
 
 export const firebaseInstance = firebase;
-
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [nweet, setNweet] = useState(""); //빈문자열
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event; //event로 부터
+        //즉, event 안에 있는 target안에 있는 value를 달라고 하는것
+        setNweet(value);
+    }
+
     return (
         <div>
-            Home
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="what's on your mind" maxLength={120} />
+                <input type="submit" value="Nweet"/>
+            </form>
         </div>
     );
 };
